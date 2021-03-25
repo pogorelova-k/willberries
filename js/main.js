dd6a7ec7720e2429e4a073f4b7df8378bfbf8862
pogorelova-k <71893791+pogorelova-k@users.noreply.github.com>
2021-03-25 17:32:13 +0300

    Корзина товаров
    - формирование корзины, с помощью из JS
    - получение необходимых элементов для нее
    - и работа с элементами в корзине (добавить или убрать товар)
    - добавлен счётчик возле корзины(количество товаров в корзине)
    - добавлена кнопка в корзине для очищения корзины
    - стирает данные из корзины по кнопке 'clear cart' (недоработано)

--- js/main.js
+++ js/main.js
@@ -13,8 +13,199 @@ const mySwiper = new Swiper('.swiper-container', {
 const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.querySelector('#modal-cart');
 const modalClose = document.querySelector('.modal-close');
+const cartTableGoods = document.querySelector('.cart-table__goods');
+const cardTableTotal = document.querySelector('.card-table__total');
+const cartCount = document.querySelector('.cart-count');
+const deleteGoods = document.querySelector('.delete-goods');
+let totalCount;
+
+//* ассинхронная функция, которая получает товары с сервера
+// Сервер, в данном случае - готовый файл db/db.json: в основном сервера в таком формате
+// await говорит программе, сделаать сначало то, что находится справа, а потом только всё остальное
+// fetch - это API, которые встроены в браузер и умеет запрашивать данные с сервера по определённому URL и возвращать промисы
+// промисы(promise) - это обещание, что придёт ответ от сервера; может быть true/false
+const getGoods = async () =>  {
+	// todo получаем не промис, а ответ от сервера за счёт await
+	const result = await fetch('db/db.json');
+	if (!result.ok) {
+		// выполним исключение
+		throw 'Ошибочка вышла:' + result.status;
+	}
+	return await result.json();
+}
+// промисы обрабатывают с помощью метода then
+// функция вызовется, когда выполнится then 
+// getGoods().then(function (data) {})
+
+// ? как по другому можно использовать fetch 
+// fetch('db/db.json')
+// 	.then(function (response) {
+// 		return response.json()
+// 	})
+// 	.then (function (data) {
+// 		console.log(data);
+// 	})
+
+
+//* пишем свои методы для формирования корзины
+const cart = {
+	//* товары в корзине
+	cartGoods: [],
+
+	//* метод для отображения товаров в корзине
+	renderCart(){
+		//todo удаляем товары из корзины
+		cartTableGoods.textContent = '';
+		//todo формируем товары заново
+		// this будет обращаться к объекту cart. this=cart
+		this.cartGoods.forEach(({id, name, price, count}) => {
+			//todo создаем строку tr
+			const trGood = document.createElement('tr');
+			trGood.className = 'cart-item';
+			trGood.dataset.id = id;
+
+			//todo вставляем в верстку
+			trGood.innerHTML = `
+				<td>${name}</td>
+				<td>${price}$</td>
+				<td><button class="cart-btn-minus">-</button></td>
+				<td>${count}</td>
+				<td><button class="cart-btn-plus">+</button></td>
+				<td>${price * count}$</td>
+				<td><button class="cart-btn-delete">x</button></td>
+			`;
+			//todo вставляем на страницу
+			cartTableGoods.append(trGood);
+		});
+
+		//todo вывод итоговой суммы 
+		// перебираем cartGoods с помощью reduce 
+		// reduce - в функции которая внутри принмает на один аргумент больше (некий аккамулятор), который скалдывает и передает в следующую итерацию цикла. получаем акк от той ф-ции, которая завершилась до этого 
+		const totalPrice = this.cartGoods.reduce((sum, item) => {
+			return (sum + item.price * item.count);
+		}, 0);
+
+		cardTableTotal.textContent = totalPrice + '$';
+
+		// выводим общее количество товаров на кнопке с корзиной при открытом модальном окне
+		cartCount.textContent = totalCount;
+	},
+
+	//* метод получает товар по id и удаляет его
+	deleteGood(id){
+		//todo  удалять ненужные объекты, остальные возвращать. с помощью filter
+		// если id будут разные, то элемент вернётся в корзину, иначе игнор 
+		this.cartGoods = this.cartGoods.filter(item => id !== item.id)
+		this.renderCart();
+	},
+	//* + и - в корзине
+	minusGood(id){
+		for (const item of this.cartGoods) {
+			if (item.id === id) { // прибавляем товар, если совпали id
+				if (item.count <= 1) { // удаляем товар, если он один или меньше
+					this.deleteGood(id);
+				} else {
+					item.count--;
+					totalCount--;
+				}
+				
+				break; 
+			}
+		};
+		this.renderCart();
+	},
+	plusGood(id){
+		for (const item of this.cartGoods) {
+			if (item.id === id) { // прибавляем товар, если совпали id
+				item.count++;
+				totalCount++;
+				break; 
+			}
+		};
+		this.renderCart();
+	},
+	//* добавлять товар в корзину с сайта
+	addCartGoods(id){
+		// find - как только находит товар, его возвращает
+		const goodItem = this.cartGoods.find(item => item.id === id);
+		if (goodItem) {
+			this.plusGood(id)
+		} else {
+			// получаем с сервера с помощью getGoods
+			getGoods()
+				//todo ищем в массиве данных с сервера товар(элемент)
+				// если находим передаём в следующий then 
+				.then(data => data.find(item => item.id === id)) 
+				// todo добавляем найденный товар в массив товаров корзины cartGoods
+				.then(({id, name, price}) => {
+					// push - добавляет элемент в массив
+					this.cartGoods.push({
+						id,
+						name,
+						price,
+						count: 1,	
+					});
+				});
+		};
+
+		// Общее количество товаров в корзине
+		totalCount = this.cartGoods.reduce((sum, item) => {
+			if (id === item.id) {
+				return sum * item.count;
+			} else {
+				return sum + item.count;
+			}
+			
+		}, 1);
+		// выводим общее количество товаров на кнопке с корзиной при закрытом модальном окне
+		cartCount.textContent = totalCount;		
+	},
+
+	//* метод удаления всех товаров из корзины
+	deleteAllGods(){
+		cartGoods: []; // очищаем массив. не получается
+		cartTableGoods.textContent = ''; // очищаем строки с товарами из корзины
+		cardTableTotal.textContent = ''; // очищаем тотал прайс
+		cartCount.textContent = ''; //очищаем кол-во всех товаров на кнопке cart 
+	}
+};
+
+//* очищаем корзину по кнопке
+deleteGoods.addEventListener('click', cart.deleteAllGods);
+
+//* добавляем товары в корзину по кнопке с ценой на сайте
+document.body.addEventListener('click', (event) => {
+	const addToCart = event.target.closest('.add-to-cart');
+	if (addToCart) {
+		cart.addCartGoods(addToCart.dataset.id)		
+	}
+})
+
+//* действия при открывании модального окна корзины
+cartTableGoods.addEventListener('click', (event) => {
+	const target = event.target;
+	// получаем id 
+	if (target.tagName === "BUTTON") {
+		// получаем родителя и его id
+		const id = target.closest('.cart-item').dataset.id;
+		//todo Удаляем товар полностью из корзины
+		if (target.classList.contains('cart-btn-delete')) {
+			cart.deleteGood(id);
+		};
+		//todo уменьшаем кол-во товаров на 1
+		if (target.classList.contains('cart-btn-minus')) {
+			cart.minusGood(id);
+		};
+		//todo увеличиваем кол-во товаров на 1
+		if (target.classList.contains('cart-btn-plus')) {
+			cart.plusGood(id);
+		};
+	};
+	
+})
 
 const openModal = () => {
+	cart.renderCart();
 	modalCart.classList.add('show');
 };
 const closeModal = () => {
@@ -71,35 +262,6 @@ const longGoodsList = document.querySelector('.long-goods-list');
 const moreAccessories = document.querySelector('.more-accessories');
 const moreClothing = document.querySelector('.more-clothing');
 
-//* ассинхронная функция, которая получает данные с сервера
-// Сервер, в данном случае - готовый файл db/db.json: в основном сервера в таком формате
-// await говорит программе, сделаать сначало то, что находится справа, а потом только всё остальное
-// fetch - это API, которые встроены в браузер и умеет запрашивать данные с сервера по определённому URL и возвращать промисы
-// промисы(promise) - это обещание, что придёт ответ от сервера; может быть true/false
-const getGoods = async function() {
-	// todo получаем не промис, а ответ от сервера
-	const result = await fetch('db/db.json');
-	if (!result.ok) {
-		// выполним исключение
-		throw 'Ошибочка вышла:' + result.status;
-	}
-	return await result.json();
-}
-// промисы обрабатывают с помощью метода then
-// функция вызовется, когда выполнится then 
-getGoods().then(function (data) {
-
-})
-
-// ? как по другому можно использовать fetch 
-// fetch('db/db.json')
-// 	.then(function (response) {
-// 		return response.json()
-// 	})
-// 	.then (function (data) {
-// 		console.log(data);
-// 	})
-
 // * Создание 1 карточки
 const createCard = ({label, name, img, description, id, price}) => {
 	//todo создаем оболочку для карточки и добавляем классы от бутсрапа
@@ -126,7 +288,7 @@ const createCard = ({label, name, img, description, id, price}) => {
 }
 
 // *функция, которая будет показывать карточкИ, вызывая createCard
-const renderCards = function (data) {
+const renderCards =  (data) => {
 	//todo стираем всё,что есть в списке товаров
 	longGoodsList.textContent = '';
 	//todo создаём элементы и передаем туда объекты
@@ -153,7 +315,7 @@ const renderCards = function (data) {
 	6 отправвляем на страницу (в longGoodsList)
 */
 
-//* открытие всех товаров
+//* открытие всех товаров по кнопкам ALL и view all
 window.addEventListener('click', (event) => {
 	const target = event.target;
 	if (target === more || target.textContent === 'All') {
@@ -171,10 +333,9 @@ const filterCards = (field, value) => {
 	getGoods()
 		.then((data) => {
 			// filter - если (good[field] === value)-true, тогда товар будет добавлен в массив отфильтрованных товаров (filteredGoods), иначе следующая итерация
-			const filteredGoods = data.filter((good) => {
+			const filteredGoods = data.filter(good => {
 				// сравнивает любое свойство у товара с value 
 				return good[field] === value;
-				
 			});
 			return filteredGoods;
 		})
@@ -194,10 +355,10 @@ navigationLink.forEach((link) => {
 })
 
 moreAccessories.addEventListener('click', (event) => {
-	(filterCards('category', 'Accessories'));
+	filterCards('category', 'Accessories');
 	scroll(event);
 })
 moreClothing.addEventListener('click', (event) => {
-	(filterCards('category', 'Clothing'));
+	filterCards('category', 'Clothing');
 	scroll(event);
 })
